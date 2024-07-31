50f87dbe6d17b80feef94f1f1ea271d83804657e
serverbeit <151661368+andihndkk@users.noreply.github.com>
2024-07-31 15:02:58 +0700

    Add project files.

---
+++ ServicesSolution/DBkonteks .cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+public class DBkonteks : DbContext
+{
+    public DBkonteks(DbContextOptions<DBkonteks> options)
+        : base(options)
+    {
+    }
+    public DbSet<inventory> Inventories { get; set; }
+
+
+    }
+
---
+++ ServicesSolution/inventory.cs
@@ -0,0 +1,10 @@
+﻿public class inventory
+{
+    public int IdInventory { get; set; }
+    public string NamaBarang { get; set; }
+    public decimal HargaBarang { get; set; }
+    public DateTime TanggalPengadaan { get; set; }
+    public int Jumlah { get; set; }
+    public string Kategori { get; set; }
+    public string Manufakturer { get; set; }
+}
